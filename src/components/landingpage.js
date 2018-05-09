61b348a5d23005d73ee7004b2f2de78b17e8bd38
Rondale W. Davis <rdavis2005@msn.com>
2018-05-09 21:20:36 +0000

    styled landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class Landing extends Component {
   render() {
     return(
-      <div><h1>Landing Page</h1></div>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img 
+              src="https://openclipart.org/image/2400px/svg_to_png/277084/Male-Avatar-3.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            
+            <div className="banner-text">
+              <h1>Junior Web Developer</h1>
+              
+            <hr/>
+            
+              <p>HTML/CSS | Bootstrap | Javascript | React | Ruby | Rails</p>
+              
+            <div className="social-links">
+            
+              {/* LinkedIn */}
+              <a href="https://www.linkedin.com/in/rondaledavis/" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-linkedin-square" aria-hidden="true" />
+              </a>
+              
+              {/* GitHub */}
+              <a href="https://github.com/RDavis2005" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-github-square" aria-hidden="true" />
+              </a>
+              
+              {/* BitBucket */}
+              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-bitbucket-square" aria-hidden="true" />
+              </a>
+              
+            </div>
+              </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
